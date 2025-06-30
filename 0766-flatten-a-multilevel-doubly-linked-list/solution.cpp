066d035d5bf076f74bee958c69498f6d9dc6706a
Yash <yashmungra191@gmail.com>
2025-06-30 10:50:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.8 MB (45.00%)

---
+++ 0766-flatten-a-multilevel-doubly-linked-list/solution.cpp
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+*/
+
+class Solution {
+public:
+    Node* flatten(Node* head) {
+        if(head == NULL) return head;
+        Node* curr = head;
+
+        while(curr != NULL){
+            if(curr->child != NULL){
+                Node* next = curr->next;
+                curr->next = flatten(curr->child);
+                curr->next->prev = curr;
+                curr->child = NULL;
+
+
+                while(curr->next!=NULL){
+                    curr = curr->next;
+                }
+                if(next!=NULL){
+                curr->next = next;
+                next->prev = curr;
+                }
+            }
+            curr = curr->next;
+        }
+        return head;
+    }
+
+};
