0751c7fd15ade60ef4a08b3a5d84bf4eae4ca9a4
Yash <yashmungra191@gmail.com>
2025-09-06 04:34:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 31.4 MB (46.20%)

--- 0792-binary-search/solution.cpp
+++ 0792-binary-search/solution.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    int bin(vector<int>& nums, int target,int st,int end){
-        if(st<=end){
-        int mid = st + (end-st)/2;
-        
-        if(nums[mid]==target)return mid;
-        else if(nums[mid]>target) return bin(nums, target, st, mid-1);
-        else return bin(nums, target, mid+1, end);
+    int search(vector<int>& nums, int target) {
+        int st = 0, end = nums.size()-1;
+
+        while(st<=end){
+            int mid = st + (end-st)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target){
+                end = mid-1;
+            } else{
+                st = mid+1;
+            }
         }
         return -1;
-    }
-
-    int search(vector<int>& nums, int target) {
-        return bin(nums,target,0,nums.size()-1);
     }
 };
