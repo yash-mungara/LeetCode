f7e0af393ae5a7f8aa20eb2732d10868f6250639
Yash <yashmungra191@gmail.com>
2025-10-29 14:01:23 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.5 MB (93.11%)

--- 0050-powx-n/solution.cpp
+++ 0050-powx-n/solution.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
-    double helper(double x, long long n) {
-        if (n == 0) return 1.0;
-
-        double half = helper(x, n / 2);
-
-        if (n % 2 == 0)
-            return half * half;
-        else
-            return half * half * x;
+    double helper(double x, long long n, double base){
+        if(n==0) return 1;
+        double ans = helper(x,n/2,base);
+        ans = ans*ans;
+        if(n%2!=0) ans = ans*base;
+        return ans;
     }
-
     double myPow(double x, int n) {
-        long long N = n; // use long long to handle n = INT_MIN safely
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
-        }
-        return helper(x, N);
+        long long N = n;
+        if(N<0) {x= 1/x;N=N*-1;}
+        double ans = helper(x,N,x);
+        return ans;
     }
 };
-
