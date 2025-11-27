9b59bb3aea8578fd9bd229da462ef836e112367d
Yash <yashmungra191@gmail.com>
2025-11-27 10:20:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.9 MB (93.46%)

--- 0039-combination-sum/solution.cpp
+++ 0039-combination-sum/solution.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    set<vector<int>> s;
-    void help(vector<int>& arr, int target,int idx,vector<int> &comb,vector<vector<int>>& ans){
-        if(idx == arr.size() || target<0){
-            return ;
-        }
-
-        if(target == 0){
-            if(s.find(comb) == s.end()){
-            ans.push_back({comb});
-            s.insert(comb);
-            }
+    void helper(vector<int>& candidates, int target, int i, vector<vector<int>>& ans,vector<int>& giver){
+        if(target==0) {
+            ans.push_back(giver);
             return;
         }
-        comb.push_back(arr[idx]);
-        help(arr,target-arr[idx],idx+1,comb,ans);
-        help(arr,target-arr[idx],idx,comb,ans);
-        comb.pop_back();
-        help(arr,target,idx+1 ,comb,ans);
+        if(i>=candidates.size() || target<0){
+            return;
+        }
+        giver.push_back(candidates[i]);   
+        helper(candidates, target - candidates[i], i, ans, giver);
+        giver.pop_back();
+        
+        helper(candidates, target, i+1, ans, giver);
     }
-
-    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
-        vector<int> comb;
-        help(arr,target,0,comb,ans);
+        vector<int> giver;
+        helper(candidates, target, 0, ans, giver);
         return ans;
     }
 };
