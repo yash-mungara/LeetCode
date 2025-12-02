e6dd621dd5195a72e1a5e9550db2367a4776b8b6
Yash <yashmungra191@gmail.com>
2025-12-02 09:58:32 +0000

    Sync LeetCode submission Runtime - 310 ms (63.87%), Memory - 8.7 MB (71.03%)

--- 0037-sudoku-solver/solution.cpp
+++ 0037-sudoku-solver/solution.cpp
@@ -1,48 +1,47 @@
 class Solution {
 public:
-    bool issafe(vector<vector<char>>& board,int row,int col,char i){
-        for(int j=0;j<9;j++){
-            if(board[j][col]==i){
-                return false;
-            }
+    bool isValid(vector<vector<char>>& board, int row, int col, char ch) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == ch) return false;  // check row
+            if (board[i][col] == ch) return false;  // check column
         }
-        for(int j=0;j<9;j++){
-            if(board[row][j]==i){
-                return false;
+
+        int r = (row / 3) * 3;
+        int c = (col / 3) * 3;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[r + i][c + j] == ch) return false;
             }
         }
 
-        int srow = (row/3)*3, scol = (col/3)*3;
-        for(int p=srow;p<=srow+2;p++){
-            for(int q=scol;q<=scol+2;q++){
-                if(board[p][q]==i)return false;
-            }
-        }
         return true;
     }
-    bool helper(vector<vector<char>>& board,int row,int col){
-        if(row==9)return true;
-        int nrow = row, ncol= col+1;
-        if(ncol==9){
-            nrow++;
-            ncol=0;
-        }
 
-        if(board[row][col]!='.'){
-            return helper(board,nrow,ncol);
-        }
-        
-        for(char i='1';i<='9';i++){
-            if(issafe(board,row,col,i)){
-                board[row][col] = i;
-                if(helper(board,nrow,ncol)){return true;}
-                board[row][col] = '.';
+    bool solve(vector<vector<char>>& board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+
+                    for (char ch = '1'; ch <= '9'; ch++) {
+                        if (isValid(board, i, j, ch)) {
+                            board[i][j] = ch;
+
+                            if (solve(board)) return true;
+
+                            board[i][j] = '.';  // backtrack
+                        }
+                    }
+
+                    return false; // no number works
+                }
             }
         }
-        return false;
+        return true; // solved
     }
+
     void solveSudoku(vector<vector<char>>& board) {
-        helper(board,0,0);
-        
+        solve(board);
     }
 };
+
