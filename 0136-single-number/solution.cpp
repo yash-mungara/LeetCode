6ab983059682d4131369f2fe502ed4cbbc3139b1
Yash <yashmungra191@gmail.com>
2025-12-06 11:29:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.5 MB (83.72%)

--- 0136-single-number/solution.cpp
+++ 0136-single-number/solution.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]!=nums[i-1] && nums[i]!=nums[i+1]) return nums[i];
+        int x = 0;
+        for(int y : nums){
+            x ^= y;
         }
-        return nums[0]; 
+        return x;
     }
 };
