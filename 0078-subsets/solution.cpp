6d19caed6d2afc19db0033dd8b3d9274afb8d963
Yash <yashmungra191@gmail.com>
2025-12-06 12:06:42 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10 MB (60.25%)

--- 0078-subsets/solution.cpp
+++ 0078-subsets/solution.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    void a(vector<int>& nums,vector<vector<int>>& set,int i,vector<int>& ans){
-        if(i==nums.size()){
-            set.push_back({ans});
-            return;
-        }
-
-        ans.push_back(nums[i]);
-        a(nums,set,i+1,ans);
-
-        ans.pop_back();
-        a(nums,set,i+1,ans);
-        
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> set;
-        vector<int> ans;
+        int n = nums.size();
+        int total = 1<<n;  
+        vector<vector<int>>  ans;
 
-        a(nums,set,0,ans);
-        return set;
+        for(int mask=0;mask<total;mask++){
+            vector<int> subset;
+            for(int i=0;i<n;i++){
+                if(mask & (1<<i)) subset.push_back(nums[i]);
+            }
+            ans.push_back(subset);
+        }
+        return ans;
     }
 };
