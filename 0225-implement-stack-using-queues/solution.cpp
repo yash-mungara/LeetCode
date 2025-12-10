2e14d1d313ee986786fb9808dc8d619477ad008d
Yash <yashmungra191@gmail.com>
2025-12-10 09:43:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.4 MB (92.03%)

--- 0225-implement-stack-using-queues/solution.cpp
+++ 0225-implement-stack-using-queues/solution.cpp
@@ -1,35 +1,30 @@
 class MyStack {
 public:
-    queue<int> q1;
-    queue<int> q2;  
+    
+    queue<int> mystack;
     MyStack() {
-        
     }
     
     void push(int x) {
-        while(!q1.empty()){
-            q2.push(q1.front());
-            q1.pop();
-        }
-        q1.push(x);
-        while(!q2.empty()){
-            q1.push(q2.front());
-            q2.pop();
+        mystack.push(x);
+        for(int i=1;i<mystack.size();i++){
+            mystack.push(mystack.front());
+            mystack.pop();
         }
     }
     
     int pop() {
-        int ans = q1.front();
-        q1.pop();
-        return ans;
+        int temp = mystack.front();
+        mystack.pop();
+        return temp;
     }
     
     int top() {
-        return q1.front();
+        return mystack.front();
     }
     
     bool empty() {
-        return q1.empty();
+        return mystack.size()==0;
     }
 };
 
