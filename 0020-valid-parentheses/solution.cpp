ad78b4a96613d0e11ab2cc2a207cfb98420810c9
Yash <yashmungra191@gmail.com>
2025-12-10 14:17:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.8 MB (67.84%)

--- 0020-valid-parentheses/solution.cpp
+++ 0020-valid-parentheses/solution.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> a;
+        stack<char> st;
+        
+        for(char c : s) {
+            // 1. If opening bracket, push
+            if(c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            }
+            else {
+                // 2. If closing bracket, stack must not be empty
+                if(st.empty()) return false;
 
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
-                a.push(s[i]);
-            } else{
-                if(a.empty()){return false;}
-
-                if(a.top() == '[' && s[i]== ']' || a.top() == '(' && s[i] == ')' || a.top() == '{' && s[i] =='}' ){
-                    a.pop();
+                // 3. Check matching pair
+                char top = st.top();
+                if((c == ')' && top != '(') ||
+                   (c == '}' && top != '{') ||
+                   (c == ']' && top != '['))
+                {
+                    return false;
                 }
-                else return false;
+                
+                st.pop();
             }
         }
-        return a.empty() ? true:false ;
+        
+        // 4. Must be empty at end
+        return st.empty();
     }
 };
+
