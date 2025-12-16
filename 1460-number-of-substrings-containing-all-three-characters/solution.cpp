fdd972838a8a83ab7dc62815dcab958cf6080036
Yash <yashmungra191@gmail.com>
2025-12-16 09:14:02 +0000

    Sync LeetCode submission Runtime - 11 ms (40.54%), Memory - 10.8 MB (98.51%)

---
+++ 1460-number-of-substrings-containing-all-three-characters/solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int lastseen[3]={-1,-1,-1};  int cnt=0;
+        for(int i=0;i<s.size();i++){
+               lastseen[s[i]-'a']=i;
+               cnt+=1+min({lastseen[0],lastseen[1],lastseen[2]});
+        }
+        return cnt;
+    }
+};
