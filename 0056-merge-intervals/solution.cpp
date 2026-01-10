74be902ba801ea1077b631b7c682f0122e3cae43
Yash <yashmungra191@gmail.com>
2026-01-10 12:31:19 +0000

    Sync LeetCode submission Runtime - 3 ms (89.05%), Memory - 23.9 MB (30.91%)

--- 0056-merge-intervals/solution.cpp
+++ 0056-merge-intervals/solution.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& arr) {
+    vector<vector<int>> merge(vector<vector<int>>& a) {
         vector<vector<int>> ans;
-        int n= arr.size();
-        sort(arr.begin(),arr.end());
-        for(int i=0;i<n;i++){
-            if(i>0 && arr[i][0]<=ans[ans.size()-1][1]){
-                if(ans[ans.size()-1][1]<arr[i][1]){
-                ans[ans.size()-1][1] = arr[i][1];
+            sort(a.begin(),a.end(),[](const vector<int> &b, const vector<int> &c){
+                return b[0]<c[0];
+            });
+            ans.push_back(a[0]);
+            for(int i=1;i<a.size();i++){
+                if(a[i][0]>ans[ans.size()-1][1]) ans.push_back(a[i]);
+                else{
+                    int n=ans.size();
+                    ans[n-1][1] = max(ans[n-1][1],a[i][1]);
                 }
-            }else {
-                ans.push_back({arr[i][0],arr[i][1]});
             }
-        }
-        return ans;
+            return ans;
     }
 };
