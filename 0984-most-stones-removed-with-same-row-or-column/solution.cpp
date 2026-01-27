2a5a67fbe94c117fc1529b76ba5bcb7e08cc706c
Yash <yashmungra191@gmail.com>
2026-01-27 09:58:39 +0000

    Sync LeetCode submission Runtime - 15 ms (72.33%), Memory - 25.5 MB (14.77%)

---
+++ 0984-most-stones-removed-with-same-row-or-column/solution.cpp
@@ -0,0 +1,73 @@
+class DisjointSet {
+    vector<int> rank, parent, size;
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1,1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int maxRow = 0, maxClm = 0, n=stones.size();
+        for(auto it:stones){
+            maxRow = max(maxRow,it[0]);
+            maxClm = max(maxClm,it[1]);
+        }
+
+        DisjointSet ds(maxRow+maxClm+1);
+        unordered_map<int,int> m;
+        int cnt=0;
+
+        for(auto it:stones){
+            int nodeR = it[0], nodeC = it[1]+maxRow+1;
+            ds.unionBySize(nodeR,nodeC);
+            m[nodeR] = 1;
+            m[nodeC] = 1;
+        }
+        for(auto it:m){
+            if(ds.findUPar(it.first) == it.first) cnt++;
+        }
+        return n-cnt;
+    }
+};
