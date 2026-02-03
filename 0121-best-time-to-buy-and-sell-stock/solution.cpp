92e47457a6be509b7c982bfd4ac4961c3bee43e7
Yash <yashmungra191@gmail.com>
2026-02-03 17:49:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.1 MB (99.99%)

--- 0121-best-time-to-buy-and-sell-stock/solution.cpp
+++ 0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -1,13 +1,38 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int mp = 0, bb = prices[0], n = prices.size();
+    int maxProfit(vector<int>& a) {
+        int n=a.size();
+        int mini  = a[0], profit = 0;
         for(int i=1;i<n;i++){
-            if(bb<prices[i]){
-                mp = max(mp,prices[i]-bb);
-            }
-            bb = min(bb,prices[i]);
+            profit = max(a[i]-mini,profit);
+            mini = min(mini,a[i]);
         }
-    return mp;
+        return profit;
     }
 };
+static int __init = [] {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    freopen("user.out", "w", stdout);
+    string line;
+    while (getline(cin, line))
+    {
+        int minPrice = INT_MAX, best = 0;
+        for (size_t i = 0, n = line.size(); i < n; )
+        {
+            while (i < n && (line[i] & 15) > 9) ++i;
+            if (i >= n) break;
+            int price = line[i++] & 15;
+            while (i < n && (line[i] & 15) < 10)
+                price = price * 10 + (line[i++] & 15);
+            if (price < minPrice)
+                minPrice = price;
+            else
+            {
+                int profit = price - minPrice;
+                if (profit > best) best = profit;
+            }
+        }
+        cout << best << "\n";
+    }
+    return 0;
+}();
