4af748840ee4ad458e60c04515b45cb0795aa03d
Yash <yashmungra191@gmail.com>
2026-02-28 06:12:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 16.7 MB (86.37%)

--- 1036-rotting-oranges/solution.cpp
+++ 1036-rotting-oranges/solution.cpp
@@ -1,43 +1,34 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n= grid.size(), m=grid[0].size();
-        queue<pair<pair<int,int>,int>> q;
-        vector<vector<int>> vis(n, vector<int>(m, 0));
-        int cntf=0;
-
+        int n= grid.size();
+        int m= grid[0].size();
+        queue<pair<pair<int,int>,int>> q;       // {{i,j},time}
+        vector<int> cr = {1,0,-1,0}, cc = {0,1,0,-1};
+        int ans=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    q.push({{i,j},0});
-                    vis[i][j]=2;
-                } else vis[i][j] = 0;
-                if(grid[i][j] == 1) cntf++;
+                if(grid[i][j]==2) q.push({{i,j},0});
             }
         }
 
-        int tm=0;
-        int drow[] = {-1,0,1,0};
-        int dclm[] = {0,-1,0,1};
-        int cnt=0;
         while(!q.empty()){
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int t = q.front().second;
-            tm = max(tm,t);
+            ans = q.front().second; int i= q.front().first.first, j= q.front().first.second;
             q.pop();
-            for(int i=0;i<4;i++){
-                int nrow = r + drow[i];
-                int nclm = c + dclm[i];
-                if(nrow>=0 && nrow<n && nclm>=0 && nclm<m && vis[nrow][nclm]!=2 && 
-                grid[nrow][nclm]==1){
-                    q.push({{nrow,nclm},t+1});
-                    vis[nrow][nclm] = 2;
-                    cnt++;
+            for(int k=0;k<4;k++){
+                int ni = i + cr[k], nj = j + cc[k];
+                if(ni>=0 && ni<n && nj>=0 && nj<m && grid[ni][nj]==1){
+                    grid[ni][nj] = 2;
+                    q.push({{ni,nj},ans+1});
                 }
             }
         }
-        if(cnt!=cntf) return -1;
-        else return tm;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1) return -1;
+            }
+        }
+        return ans;
     }
 };
