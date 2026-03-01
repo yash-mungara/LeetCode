49e1c2fbbba8e4c4071f982d5d0bad926855496d
Yash <yashmungra191@gmail.com>
2026-03-01 05:03:55 +0000

    Sync LeetCode submission Runtime - 15 ms (27.10%), Memory - 40.4 MB (8.74%)

--- 2090-number-of-ways-to-arrive-at-destination/solution.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/solution.cpp
@@ -1,41 +1,35 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int,int>>> adj(n);
+        const int MOD = 1e9 + 7;
+        vector<vector<pair<long long,int>>> adj(n);
         for(auto x: roads){
             adj[x[0]].push_back({x[2], x[1]});
             adj[x[1]].push_back({x[2], x[0]});
         }
 
-        set<pair<long long,int>> s;   // 🔧 long long
+        priority_queue<pair<long long, int>,vector<pair<long long, int>>,greater<pair<long long, int>>>pq;   // 🔧 long long
         vector<long long> dis(n, 1e18); // 🔧 long long
-        vector<int> ways(n, 0);
+        vector<long long> ways(n, 0);
 
-        int m = 1e9 + 7;
-
-        s.insert({0, 0});
-        dis[0] = 0;
         ways[0] = 1;
+        dis[0] = 0;
+        pq.push({0,0});
 
-        while(!s.empty()){
-            auto [dist, node] = *s.begin();
-            s.erase(s.begin());
+        while(!pq.empty()){
+            auto node = pq.top().second;
+            auto dist = pq.top().first;
+            pq.pop();
 
-            if(dist > dis[node]) continue;  // 🔧 safety check
+            for(auto x:adj[node]){
+                auto ndis = dist + x.first;
 
-            for(auto x : adj[node]){
-                long long nd = dist + x.first;
-
-                if(dis[x.second] > nd){
-                    if(dis[x.second] != 1e18)
-                        s.erase({dis[x.second], x.second});
-
-                    dis[x.second] = nd;
+                if(ndis<dis[x.second]){
                     ways[x.second] = ways[node];
-                    s.insert({nd, x.second});
-                }
-                else if(dis[x.second] == nd){
-                    ways[x.second] = (ways[x.second] + ways[node]) % m;
+                    dis[x.second] = dist+x.first;
+                    pq.push({dis[x.second],x.second});
+                } else if(ndis==dis[x.second]){
+                    ways[x.second] = (ways[x.second]+ways[node])%MOD;
                 }
             }
         }
