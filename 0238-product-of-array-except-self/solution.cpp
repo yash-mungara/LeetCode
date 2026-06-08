a43ab5213afdc021373cccf3efb7de3fb9422ba8
Yash <yashmungra191@gmail.com>
2026-06-08 08:45:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 40.3 MB (61.08%)

--- 0238-product-of-array-except-self/solution.cpp
+++ 0238-product-of-array-except-self/solution.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans(n,1);
-    
-    
-    for(int i=1;i<n;i++){
-        ans[i] *= ans[i-1]*nums[i-1];
-    }
-    int x = 1;
-    for(int i=n-2;i>=0;i--){
-        x = x*nums[i+1];
-        ans[i] *= x;
-    }
-    
-    return ans;
+        int product = 1;
+        bool iszero = false, ISzero = false;
+        for(int it:nums){
+            if(it==0){
+                if(iszero) ISzero = true;
+                iszero = true;
+            } else{
+                product *= it;
+            }
+        }
+        vector<int> ans(nums.size(), 0);
+        if(ISzero) return ans;
+        for(int i=0;i<nums.size();i++){
+            if(iszero){
+                if(nums[i]!=0) ans[i] = 0;
+                else ans[i] = product;
+            } else{
+                ans[i] = product/nums[i];
+            }
+        }
+        return ans;
     }
 };
