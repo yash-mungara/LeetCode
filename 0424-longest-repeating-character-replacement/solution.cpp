5a44bfb5e7b7402ddb25b3ad59efb5290e20abc1
Yash <yashmungra191@gmail.com>
2026-06-13 11:17:44 +0000

    Sync LeetCode submission Runtime - 3 ms (68.49%), Memory - 10.8 MB (70.94%)

--- 0424-longest-repeating-character-replacement/solution.cpp
+++ 0424-longest-repeating-character-replacement/solution.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int ans=0, i=0, j=0, maxf=0;
-        vector<int> arr(26,0);
+        int ans=0;
+        vector<int> vec(26, 0);
+        int l=0, r=0;
 
-        while(j<s.size()){
-            arr[s[j]-'A']++;
-            maxf = max(maxf,arr[s[j]-'A']);
-            
-            if(j-i+1-maxf<=k) {
-                ans = max(ans,j-i+1);
+        while(r<s.size()){
+            int maxfreq=0;
+            vec[s[r]-'A']++;
+            for(int it:vec){maxfreq = max(maxfreq, it);}
+
+            if(r-l+1 - maxfreq <=k){
+                ans = max(ans, r-l+1);
+                r++;
             } else{
-                arr[s[i]-'A']--;
-                for(int x:arr) maxf = max(maxf,x);
-                i++;
+                vec[s[l]-'A']--;
+                vec[s[r]-'A']--;
+                l++;
             }
-            j++;
         }
         return ans;
     }
