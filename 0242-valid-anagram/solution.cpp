0544c8cdd482c29c23c9aee51368f770d6329369
Yash <yashmungra191@gmail.com>
2026-06-13 05:35:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.8 MB (25.70%)

--- 0242-valid-anagram/solution.cpp
+++ 0242-valid-anagram/solution.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t) return true;
-        else return false;
+        if(s.size() != t.size()) return false;
+        vector<int> S(26,0), T(26,0);
+        
+        for(int i=0;i<s.size();i++){
+            S[s[i]-'a']++; T[t[i]-'a']++;
+        }
+
+        for(int i=0;i<26;i++){
+            if(S[i]!=T[i]) return false;
+        }
+        return true;
     }
 };
