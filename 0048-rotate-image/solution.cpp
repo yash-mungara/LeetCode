54ef0805e5ecb3869590a34f7173db635f10cfe1
Yash <yashmungra191@gmail.com>
2026-06-26 05:09:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.2 MB (37.26%)

--- 0048-rotate-image/solution.cpp
+++ 0048-rotate-image/solution.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        for(int i=0;i<matrix.size();i++){
-            for(int j=i;j<matrix.size();j++){
-                swap(matrix[i][j],matrix[j][i]);
+        int n = matrix.size();
+        for(int i=0;i<n;i++){
+            for(int j=i;j<n;j++){
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
-        int st=0, end=matrix.size()-1;
-        for(int k=0;k<matrix.size();k++){
-            while(st<=end){
-                swap(matrix[k][st],matrix[k][end]);
-                st++;end--;
-            }
-            st=0;
-            end=matrix.size()-1;
+
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
