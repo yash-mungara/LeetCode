f9a86a7854c198679e6ee354cfb4831df484a2d4
Yash <yashmungra191@gmail.com>
2026-06-27 06:07:59 +0000

    Sync LeetCode submission Runtime - 27 ms (29.13%), Memory - 48.1 MB (28.90%)

--- 1442-number-of-operations-to-make-network-connected/solution.cpp
+++ 1442-number-of-operations-to-make-network-connected/solution.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
-    int find(int u, vector<int> &parent){
-        if(parent[u]==u) return u;
-        else return parent[u] = find(parent[u], parent);
+    vector<int> parent, size;
+
+    int findpar(int node){
+        if(parent[node]==node) return node;
+        return parent[node] = findpar(parent[node]);
+    }
+    void ubysize(int node1, int node2){
+        int p1 = findpar(node1), p2 = findpar(node2);
+        if(p1==p2) return;
+
+        if(size[p1]<size[p2]){
+            parent[p1] = p2;
+            size[p2] += size[p1];
+        } else{
+            parent[p2] = p1;
+            size[p1] += size[p2];
+        }
     }
     int makeConnected(int n, vector<vector<int>>& connections) {
-        int V = connections.size();
-        if(V<n-1) return -1;
+        if(connections.size()<n-1) return -1;
+        parent.resize(n); size.resize(n,1);
+        unordered_set<int> s;
+        for(int i=0;i<n;i++) parent[i] = i;
 
-        vector<int> parent(n);
-        int extra=0;
-        for(int i=0;i<n;i++){ parent[i]=i;}
         for(auto it:connections){
-            int u=it[0], v=it[1];
-            int prnt_u = find(u,parent);
-            int prnt_v = find(v,parent);
-            if(prnt_u == prnt_v) extra++;
-
-            else {
-                parent[prnt_v] = prnt_u;
-            }
+            ubysize(it[0], it[1]);
         }
-        
-        return n-1-(V-extra);
 
+        for(int i=0;i<n;i++){
+            int x = findpar(i);
+            s.insert(x);
+        }
+        return s.size()-1;
     }
 };
